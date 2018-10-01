8d0f6cca22c1a1e33890fc0b4ca74acc9f8dac6c
Leno Lima <lenno.lima.ferreira@gmail.com>
2018-10-01 08:00:31 -0300

    first commit

---
+++ src/screens/Auth.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class Auth extends Component {
+    state = {  }
+
+    render() {
+        return (
+            <View>
+                <Text>Login Screen</Text>
+            </View>
+        );
+    }
+}
